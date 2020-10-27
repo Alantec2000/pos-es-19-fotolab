c52dc17388a10c4c1f4fef06ec0a3406dd40075e
Alantec2000 <alansantana1995@hotmail.com>
2020-10-27 19:20:37 -0300

    feat: adicionada factory de usuário feat: Adicionadas regras de validação da requisição para criar usuário. wip: Cadastro de usuário.

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -2,8 +2,32 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Usuario;
+use App\Support\Factory\UserFactory;
+use Throwable;
+
 class UsuarioController extends Controller
 {
+    public function criar()
+    {
+        $usuario = UserFactory::make([
+            'tipo' => request()->get('tipo', 'cliente')
+        ]);
+
+        $dadosUsuario = request()->validate(
+            $usuario->criarNovoUsuarioRegras()
+        );
+
+        try {
+            $usuario->setarAtributos($dadosUsuario);
+        } catch (Throwable $e) {
+            return redirect('usuario.cadastro.erro', 400)->with('error', $e);
+        }
+
+        return redirect('usuario.cadastro.sucesso')
+            ->with('usuario', $usuario);
+    }
+
     public function listarUsuarios()
     {
         return [];
