3bbb298982cfe9a3e7a0e39c29c9f9618d59ec7d
Alantec2000 <alansantana1995@hotmail.com>
2021-03-22 15:33:49 -0300

    remoção do slick js

--- resources/views/templates/master.blade.php
+++ resources/views/templates/master.blade.php
@@ -7,10 +7,6 @@
 
         <link href="https://fonts.googleapis.com/css2?family=Kanit:ital,wght@0,200;0,300;0,400;0,500;0,600;0,700;1,200;1,300;1,400;1,600&display=swap" rel="stylesheet">
 
-        <link rel="stylesheet" href="{{ asset('css/slick.css') }}">
-        <link rel="stylesheet" href="{{ asset('css/slick-theme.css') }}">
-
-        @yield('css-view')
         <link rel="stylesheet" href="{{ asset('css/main.css') }}">
         <link rel="stylesheet" href="{{ asset('css/font.css') }}">
     </head>
